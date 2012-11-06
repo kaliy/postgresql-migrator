691fbe5a4f6c808166d639df671469f3ea631191
Arthur Kalimullin <kalimullin@gmail.com>
2012-11-06 13:31:25 +0600

    Configuration

---
+++ src/org/kaliy/migrator/Kernel.java
@@ -0,0 +1,11 @@
+package org.kaliy.migrator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Kernel {
+    private static Logger logger = LoggerFactory.getLogger(Kernel.class);
+    public static void main(String[] args) {
+        logger.info("PostgreSQL-migrator is starting...");
+    }
+}

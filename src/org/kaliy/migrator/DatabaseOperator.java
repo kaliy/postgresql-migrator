b6d6d5afce60e71ca66beb4949adaebee2ca3f95
Arthur Kalimullin <kalimullin@gmail.com>
2012-11-07 12:57:41 +0600

    Getting BLOBs

--- src/org/kaliy/migrator/DatabaseOperator.java
+++ src/org/kaliy/migrator/DatabaseOperator.java
@@ -1,19 +1,27 @@
 package org.kaliy.migrator;
 
+import org.postgresql.PGConnection;
+import org.postgresql.largeobject.LargeObject;
+import org.postgresql.largeobject.LargeObjectManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayOutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DatabaseOperator {
 
     private static Logger logger = LoggerFactory.getLogger(DatabaseOperator.class);
 
     private String host;
+
+    private static final int BUFFER_SIZE = 4096;
 
     public DatabaseOperator(String host) {
         this.host = host;
@@ -56,6 +64,46 @@ public class DatabaseOperator {
             }
         }
 
+    }
+
+    public Map<Long, byte[]> getBlobs(List<Long> ids) {
+        Connection connection = null;
+        Map<Long, byte[]> largeObjectMap = new HashMap<Long, byte[]>();
+        byte[] buf = new byte[4096];
+        try {
+            connection = DriverManager.getConnection(getConnectionUrl(host),
+                    Config.getConnectionUsername(),
+                    Config.getConnectionPassword());
+            connection.setAutoCommit(false);
+            LargeObjectManager largeObjectManager = ((PGConnection)connection).getLargeObjectAPI();
+            for(Long id: ids) {
+                logger.debug("Opening large object {}", id);
+
+                int readed;
+                int offset = 0;
+                LargeObject largeObject = largeObjectManager.open(id, LargeObjectManager.READ);
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+                while ((readed = largeObject.read(buf, offset, BUFFER_SIZE)) > 0) {
+                    byteArrayOutputStream.write(buf, 0, readed);
+                }
+                largeObjectMap.put(id, byteArrayOutputStream.toByteArray());
+                largeObject.close();
+
+                logger.info("Added BLOB {} with byte[] size {}", id, byteArrayOutputStream.size());
+            }
+        } catch (SQLException e) {
+            logger.error("Error while loading BLOBs: ", e);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    logger.error("Error while closing connection: ", e);
+                }
+            }
+        }
+        return largeObjectMap;
     }
 
     private String getConnectionUrl(String host) {
